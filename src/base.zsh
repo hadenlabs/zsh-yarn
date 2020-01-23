af02f733cbec90ca32de6930a8a866412360cf4d
@slovacus <slovacus@gmail.com>
2020-01-23 00:06:49 -0500

    (#3) chore: Implement install fzf

--- src/base.zsh
+++ src/base.zsh
@@ -1,6 +1,14 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+function fzf::exist {
+    if ! type -p fzf > /dev/null; then
+        echo 0
+        return
+    fi
+    echo 1
+}
+
 function brew::exist {
     if ! type -p brew > /dev/null; then
         echo 0
@@ -33,6 +41,14 @@ function yarn::exist {
     echo 1
 }
 
+function fzf::install {
+    if [ "$(brew::exist)" -eq 0 ]; then
+        message_warning "Please Install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+        return
+    fi
+    brew install fzf
+}
+
 function nvm::install {
     if [ "$(nvm::exist)" -eq 0 ]; then
         message_warning "Please Install nvm or use antibody bundle luismayta/zsh-nvm branch:develop"
@@ -54,6 +70,7 @@ function yarn::install {
 }
 
 
+if [ "$(fzf::exist)" -eq 0 ]; then fzf::install; fi
 if [ "$(nvm::exist)" -eq 0 ]; then nvm::install; fi
 if [ "$(node::exist)" -eq 0 ]; then node::install; fi
 if [ "$(yarn::exist)" -eq 0 ]; then yarn::install; fi
