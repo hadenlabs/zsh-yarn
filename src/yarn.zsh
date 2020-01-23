2e957119c3c831d586074ac891608186e7f67133
@slovacus <slovacus@gmail.com>
2020-01-23 01:43:57 -0500

    (#3) chore: Implement script and others data of yarn

---
+++ src/yarn.zsh
@@ -0,0 +1,38 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function yarn::package::exist {
+    if [ -f package.json ]; then
+        echo 1
+        return
+    fi
+    echo 0
+}
+
+function yarn::script::list {
+    if [ "$(yarn::package::exist)" -eq 0 ]; then
+        message_info "Not found package.json"
+        return
+    fi
+    cat package.json | jq -r ".scripts" | jq -r 'to_entries[] | [.key, .value] | @tsv'
+}
+
+function yarn::dependencies::list {
+    if [ "$(yarn::package::exist)" -eq 0 ]; then
+        message_info "Not found package.json"
+        return
+    fi
+    cat package.json | jq -r ".dependencies" | jq -r 'to_entries[] | [.key, .value] | @tsv'
+}
+
+function yarn::search::scripts {
+    local command
+    command=$(yarn::script::list \
+                  | fzf \
+                  | awk '{print $1}' \
+                  | perl -pe 'chomp'
+           )
+    if [ -n "${command}" ]; then
+        yarn run "${command}"
+    fi
+}
